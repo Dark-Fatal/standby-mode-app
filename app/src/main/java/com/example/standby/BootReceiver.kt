a9bca93da161a043590a57ddbfe2a2cdd5483ef2
Dark-Fatal <monnetmateis@gmail.com>
2026-08-11 00:50:45 +0200

    Create app/src/main/java/com/example/standby/BootReceiver.kt

---
+++ app/src/main/java/com/example/standby/BootReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.standby
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            // Redémarrer le service après un redémarrage
+            val serviceIntent = Intent(context, StandbyService::class.java)
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
